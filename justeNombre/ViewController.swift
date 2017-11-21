7fe6c15b86f451d8ab933489d9eed62c83452648
djiby3241 <djiby_alpha.sylla@utt.fr>
2017-11-21 15:57:14 +0100

    Initial Commit

---
+++ justeNombre/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  justeNombre
+//
+//  Created by if26-grp1 on 21/11/2017.
+//  Copyright © 2017 if26-grp1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var infLabel: UILabel!
+    @IBOutlet weak var supLabel: UILabel!
+    @IBOutlet weak var coupsLabel: UILabel!
+    @IBOutlet weak var saissieText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
